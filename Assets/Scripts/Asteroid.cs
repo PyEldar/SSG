bb1a6ea7e77341ce5d908e144b934eb4ecae56ab
jiri.pavlik <jiri.pavlik@kajot.cz>
2019-02-14 13:57:26 +0100

    Initial Commit, player ship movement working

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+	[SerializeField]float minScale = 0.8f;
+	[SerializeField]float maxScale = 1.2f;
+
+	[SerializeField]float rotationOffset = 40f;
+
+	Vector3 randomRotation;
+
+	void Start(){
+		Vector3 scale = Vector3.one;
+
+		scale.x = Random.Range(minScale, maxScale);
+		scale.y = Random.Range(minScale, maxScale);
+		scale.z = Random.Range(minScale, maxScale);
+
+		randomRotation.x = Random.Range(-rotationOffset, rotationOffset);
+		randomRotation.y = Random.Range(-rotationOffset, rotationOffset);
+		randomRotation.z = Random.Range(-rotationOffset, rotationOffset);
+
+		transform.localScale = scale;
+	}
+
+	void Update(){
+		transform.Rotate(randomRotation * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour {
+	[SerializeField]Asteroid asteroid;
+	[SerializeField]int asteroidsOnAxis = 10;
+	[SerializeField]int spacing = 30;
+	[SerializeField]int offset = 15;
+
+	void Start(){
+		PlaceAsteroids();
+	}
+	void PlaceAsteroids(){
+		for(int x=0; x < asteroidsOnAxis; x++){
+			for(int y=0; y < asteroidsOnAxis; y++){
+				for(int z=0; z < asteroidsOnAxis; z++){
+					CreateAsteroid(x, y, z);
+				}
+			}
+		}
+	}
+
+	void CreateAsteroid(int x, int y, int z){
+		Vector3 newPosition = new Vector3(
+			transform.position.x + (x * spacing) + CalculateOffset(),
+			transform.position.y + (y * spacing) + CalculateOffset(),
+			transform.position.z + (z * spacing) + CalculateOffset()
+		);
+		Instantiate(asteroid, newPosition, Quaternion.identity, transform);
+
+	}
+
+	float CalculateOffset(){
+		return Random.Range(-offset, offset);
+	}
+}
